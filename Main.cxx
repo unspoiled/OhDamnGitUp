ef5ffdca9c87c591dbd85de67f73736f21c7dd78
Buffalo2391 <buffalo2391@gmail.com>
2017-05-25 14:08:32 +1200

    Add files via upload

--- Main.cxx
+++ Main.cxx
@@ -25,54 +25,36 @@ bool CheckAlarm(int delay){
 	string repeat;
 	string weekday;
 	int tone;
-	struct tm * alarmTime;	
-	struct tm * currentTime;
-	struct tm * delayTime;
+	struct tm * alarmTime;
 	time_t rawtime;
-	time (&rawtime);
-	currentTime = localtime(&rawtime);
-	delayTime = localtime(&rawtime);
+	time_t localTime;
+	double adiff;
+	time(&rawtime);
 	alarmTime = localtime(&rawtime);
-	if(delay==3600){
-		delayTime->tm_hour++;
-	}else{
-		delayTime->tm_min+=delay/60;
-		delayTime->tm_sec+=delay%60;
-	}
-	mktime(currentTime);
-	mktime(delayTime);
-	mktime(delayTime);
+	localTime = mktime(localtime(&rawtime));
+	mktime(alarmTime);
 	if (inxml.is_open()){
 		inxml.ignore(200,'>');
 		inxml.ignore(200,'>');
-		while( getline (inxml,line,'>')){
-			
+		while( getline (inxml,line,'>')){			
 			inxml.ignore(200,'>');
-			getline(inxml,line,'<');
-			
-			if(line!="</Alarms"){				
-				
-				alarmTime->tm_hour = stoi(line.substr(0,1),nullptr, 10);
-				alarmTime->tm_min = stoi(line.substr(2,3),nullptr, 10);
-				
+			getline(inxml,line,'<');			
+			if(line!="</Alarms"){
+				alarmTime->tm_hour = stoi(line.substr(0,2),nullptr, 10);
+				alarmTime->tm_min = stoi(line.substr(2,2),nullptr, 10);
+				alarmTime->tm_sec = 0;
 				inxml.ignore(200,'>');
 				inxml.ignore(200,'>');				
-				getline(inxml,line,'<');
-				
+				getline(inxml,line,'<');				
 				repeat=line;
-				
 				inxml.ignore(200,'>');
 				inxml.ignore(200,'>');
 				getline(inxml,line,'<');
-				
-				weekday=line;
-				
+				weekday=line;								
 				inxml.ignore(200,'>');
 				inxml.ignore(200,'>');				
 				getline(inxml,line,'<');
-				
 				tone=stoi(line,nullptr,10);
-				
 				inxml.ignore(200,'>');
 				inxml.ignore(200,'>');
 				inxml.ignore(200,'>');
@@ -80,22 +62,29 @@ bool CheckAlarm(int delay){
 				inxml.ignore(200,'>');
 				switcher=repeat[0];
 				switch(switcher){
-					case 'd':						
-							if(difftime( mktime(alarmTime), mktime(currentTime))<difftime(mktime(delayTime), mktime(currentTime))&&difftime(mktime(alarmTime), mktime(currentTime)>0)){
-							return true;
+					case 'd':		
+							adiff=mktime(alarmTime)-localTime;
+							cout<<adiff<<"\n";							
+							if((0<adiff)&(adiff<delay)){
+								return true;
 						}
 					break;
 					case 'w':
-						if(weekday.find(to_string(currentTime->tm_wday))){
-							if(difftime( mktime(alarmTime), mktime(currentTime))<difftime(mktime(delayTime), mktime(currentTime))&&difftime(mktime(alarmTime), mktime(currentTime)>0)){
+						if(7>weekday.find(to_string(alarmTime->tm_wday))){
+							adiff=mktime(alarmTime)-localTime;
+								cout<<adiff<<"\n";
+								
+							if((0<adiff)&(adiff<delay)){
 								return true;
 							}
 						}
 					break;
 					case 'n':
-							if(difftime( mktime(alarmTime), mktime(currentTime))<difftime(mktime(delayTime), mktime(currentTime))&&difftime(mktime(alarmTime), mktime(currentTime)>0)){
-							return true;
-						}
+							adiff=mktime(alarmTime)-localTime;
+							cout<<adiff<<"\n";								
+							if((0<adiff)&(adiff<delay)){
+								return true;
+							}
 					break;
 					case 'o':
 						return false;
@@ -111,7 +100,7 @@ bool CheckAlarm(int delay){
 	else{
 		cout<< "fail";
 	}
-
+	cout<<tone;
 	
 	return false;
 }
@@ -135,18 +124,22 @@ void CheckerThread(){
 	mutex mtx;
 	unique_lock<std::mutex> lockr(mtx);
 	cout<< "Hello World\n";
-	//while(true){
+	while(true){
 		if(CheckAlarm(delay)){
-			if(delay<60){
+			if(delay<15){
 				alarm();
 				delay=3600;
+				
+			ConVar.wait_for(lockr, chrono::seconds(60));
 			} else {
 				delay=delay/2;
+				cout<<"True tho\n";
 			}
-		}else{
-			ConVar.wait_for(lockr, chrono::seconds(10));
+		} else {
+			cout<<"false thou\n";
+			ConVar.wait_for(lockr, chrono::seconds(delay));
 		}
-	//}
+	}
 	cout<<"Goodbye World\n";
 	return;
 }
@@ -155,13 +148,13 @@ int main(int argc, char **argv){
 	mutex mtx;
 	unique_lock<std::mutex> lockr(mtx);
 	thread checkT(CheckerThread);
-	//while(true){
-		ConVar.wait_for(lockr, chrono::seconds(5));
+	while(true){
+		ConVar.wait_for(lockr, chrono::seconds(40));
 		//waitforhtml
 		//makehtml
 		//waitforupdate
 		ConVar.notify_all();
-	//}
+	}
 	checkT.join();
 	cout<< "Hello From The Other Sides\n";
 	return 0;
